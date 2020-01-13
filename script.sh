ba19e11e952ac75356b9f804ad9c3036a38a21d4
Shivam Tiwari <38368497+variiable@users.noreply.github.com>
2020-01-13 08:41:19 -0500

    script uploaded
    v01

---
+++ script.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+#wget https://query1.finance.yahoo.com/v7/finance/download/GOOG?period1=1547290473&period2=1578826473&interval=1d&events=history&crumb=GR2VPGQXg0L
+touch fp.csv
+cat GOOG.csv | tr -s "," " "| cut -d " " -f1,2,3,4  > fp.csv
+read -p "please enter the date in YYYY-MM-DD format"
+echo "FORMAT: DATE OPEN HIGH LOW"
+cat fp.csv | grep -w "$REPLY"
+
+
+
+
+
