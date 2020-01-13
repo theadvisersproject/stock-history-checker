290cad8df862bcf5e8b08eebfd608ba6fb0bbb04
Shivam Tiwari <38368497+variiable@users.noreply.github.com>
2020-01-13 08:49:22 -0500

    updated directions for usage

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+#check readme file for usage information.
 #wget https://query1.finance.yahoo.com/v7/finance/download/GOOG?period1=1547290473&period2=1578826473&interval=1d&events=history&crumb=GR2VPGQXg0L
 touch fp.csv
 cat GOOG.csv | tr -s "," " "| cut -d " " -f1,2,3,4  > fp.csv
